b4a7e311edf92769577c05c88317f28fa1c12965
newtypersh <allthat1013@naver.com>
2025-12-01 16:56:45 +0900

    chore: oauth2/callback/google 경로가 swagger에 안 보이도록 수정

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -23,13 +23,7 @@ router.get(
     failureMessage: true,
   }),
   (req, res) => {
-    /* 
-       #swagger.tags = ['Auth']
-       #swagger.summary = '구글 로그인 콜백'
-       #swagger.description = '구글 인증 서버에서 돌아오는 콜백 URL입니다. 인증 성공 시 세션을 생성하고 메인 페이지(/)로 리다이렉트합니다.'
-       #swagger.responses[302] = { description: '로그인 성공 후 리다이렉트' }
-       #swagger.responses[401] = { description: '인증 실패 (로그인 페이지로 리다이렉트)' }
-    */
+    // #swagger.ignore = true
     res.redirect('/');
   }
 );
