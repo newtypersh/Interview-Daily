36d38b575d69add308543c8653f2498e587a0c12
newtypersh <allthat1013@naver.com>
2025-12-01 15:04:43 +0900

    fix: 경로 중복 수정

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -4,10 +4,10 @@ import { ensureAuth } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.get('/oauth2/login/google', passport.authenticate('google'));
+router.get('/login/google', passport.authenticate('google'));
 
 router.get(
-  '/oauth2/callback/google',
+  '/callback/google',
   passport.authenticate('google', {
     failureRedirect: '/oauth2/login/google',
     failureMessage: true,
@@ -17,7 +17,7 @@ router.get(
   }
 );
 
-router.post("/oauth2/logout", ensureAuth, function (req, res, next) {
+router.post("/logout", ensureAuth, function (req, res, next) {
   req.logout(function (err) {
     if (err) return next(err);
     res.redirect('/');
