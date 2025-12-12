6b035d4406f39cbd5e366be6f546ae4945e27629
newtypersh <allthat1013@naver.com>
2025-12-12 09:40:17 +0900

    코드 단순하게 변경

--- Front/src/react-query/queries/useFeedbackTemplate.ts
+++ Front/src/react-query/queries/useFeedbackTemplate.ts
@@ -2,15 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 import { getFeedbackTemplateByCategory } from '../../apis/feedbackTemplate/index';
 
 export const useFeedbackTemplate = (category: string | undefined | null) => {
-  const { data: templates, isLoading, error } = useQuery({
+  return useQuery({
     queryKey: ['feedbackTemplates', category],
     queryFn: () => getFeedbackTemplateByCategory(category!),
     enabled: !!category,
+    select: (data) => ({ templates: data }),
   });
-
-  return {
-    templates,
-    isLoading,
-    error,
-  };
 };
