d86570a2e8792483fa505a8a44a9851fd61c3a5d
newtypersh <allthat1013@naver.com>
2025-12-16 09:19:32 +0900

    feat: custom type 확장

---
+++ Backend/types/environment.d.ts
@@ -0,0 +1,9 @@
+namespace NodeJS {
+  interface ProcessEnv extends NodeJS.ProcessEnv {
+    PORT: string;
+    DATABASE_URL: string;
+    EXPRESS_SESSION_SECRET: string;
+    PASSPORT_GOOGLE_CLIENT_ID: string;
+    PASSPORT_GOOGLE_CLIENT_SECRET: string;
+  }
+}
