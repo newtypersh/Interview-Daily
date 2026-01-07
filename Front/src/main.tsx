25958479b3eb0a46cb79628fb896e0c98a7fb5f5
newtypersh <allthat1013@naver.com>
2026-01-07 14:52:06 +0900

    feat: react query에 broadcast 적용

--- Front/src/main.tsx
+++ Front/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { broadcastQueryClient } from '@tanstack/query-broadcast-client-experimental'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -12,6 +13,11 @@ const queryClient = new QueryClient({
       retry: 1,
     },
   },
+})
+
+broadcastQueryClient({
+  queryClient,
+  broadcastChannel: 'interview-daily',
 })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
